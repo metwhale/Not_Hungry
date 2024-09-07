ec3feb1ed369a52f156c51e811b6ad746f970da6
metwhale <67542173+metwhale@users.noreply.github.com>
2024-09-07 09:56:52 +0800

    Update ele_kpbl.py

--- ele_kpbl.py
+++ ele_kpbl.py
@@ -70,7 +70,7 @@ def xsign(api, data, uid, sid, wua, v):
 
     try:
         r = requests.post(
-            "http://192.168.1.124:1888/api/getXSign",
+            "http://192.168.124.104:9999/api/getXSign",
             # "http://127.0.0.1:18848/api/getXSign",
             json=body
         )
